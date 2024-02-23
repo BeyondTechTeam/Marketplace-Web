89b14199980a97d2bdcf810bf0bbfaaf8f36ce10
Besiwahfors <pribeson121@gmail.com>
2024-02-23 13:34:20 +0000

    firstpush

---
+++ src/Pages/Landing/index.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../../Components/Navbar/Navbar";
+import Topsellers from "../../Components/Topsellers/Topsellers";
+
+const LandingPage = () => {
+    return <div>
+        <Navbar />
+        <Topsellers />
+    </div>;
+     
+};
+ 
+export default LandingPage;
+
+// function LandingPage() {
+//     return ( 
+//         <div>LandingPage</div>
+//      );
+// }
+
+// export default LandingPage;
