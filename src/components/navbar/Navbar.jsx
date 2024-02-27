8408a0bf06c61d39359b46e67537e401ad2343c6
shari-codest <awunieunice@gmail.com>
2024-02-27 11:19:50 +0000

    update on navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import agramain from '../../assets/images/agramain.png'
+import carticon from '../../assets/images/carticon.jpeg'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+
+       <nav>
+      <img className={styles.logo} src={agramain} alt="logo" />
+      <div>
+        <Link className={styles.linktags} to="/">
+          Home
+        </Link>
+        <Link className={styles.linktags} to="/shop">
+          Shop
+        </Link>
+        <Link className={styles.linktags} to="/contact">
+          Contact
+        </Link>
+        <Link className={styles.linktags} to="/sell">
+          Sell
+        </Link>
+      </div>
+      <img className={styles.carticon} src={carticon} alt="carticon" />
+    </nav>
+ 
+  )
+}
+
+export default Navbar
