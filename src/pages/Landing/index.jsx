9bd5ea9eebae594a7ca2ff8e0959c34ef65108a4
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-02-27 13:21:26 +0000

    merge changes

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,13 +1,15 @@
-import Topsellers from "../../Components/Topsellers/Topsellers";
+import Topsellers from "../../components/Topsellers/Topsellers";
+import CategoryText from "../../components/categories/CategoryText";
+import CategoryContainer from "../../components/categories/CategoryContainer";
 
 const LandingPage = () => {
   return (
     <div>
+      <CategoryText />
+      <CategoryContainer />
       <Topsellers />
     </div>
   );
 };
 
 export default LandingPage;
-
-
