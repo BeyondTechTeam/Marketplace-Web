fa9cdac2018d70aa734e16daae199c4054b23649
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-02-27 11:36:20 +0000

    productCategories branch initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Shop from "./pages/shop/Shop";
-import LandingPage from "./pages/landing/LandingPage";
-import Contacts from "./components/contact/Contacts";
-import Sell from "./pages/sell/Sell";
-import Footer from "./components/footer/Footer";
+import LandingPage from "./pages/Landing";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/contact" element={<Contacts />} />
-        <Route path="/sell" element={<Sell />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <LandingPage />
+    </div>
   );
 }
 
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Hero from "../../components/hero/Hero";
 import CategoryText from "../../components/categories/CategoryText";
 import CategoryContainer from "../../components/categories/CategoryContainer";
 
 function LandingPage() {
   return (
     <div>
-      <Hero />
       <CategoryText />
       <CategoryContainer />
     </div>
