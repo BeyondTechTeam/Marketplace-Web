8f387162e07d543edc72e57c0623b306777db161
shari-codest <awunieunice@gmail.com>
2024-02-28 12:33:53 +0000

    add changes to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import LandingPage from "./pages/Landing";
+import LandingPage from "./Pages/Landing/Landing";
 
 function App() {
   return (
     <>
-      <LandingPage />
+     <LandingPage/>
     </>
   );
 }
