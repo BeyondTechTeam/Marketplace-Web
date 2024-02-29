7c74f433eebd214db6297783f6639a1647035ffa
DanquahSika <maamesikadanquah203@gmail.com>
2024-02-29 10:26:59 +0000

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Shop from "./Pages/shop/Shop";
 import Contacts from "./Pages/contacts/Contacts";
 import Sell from "./Pages/sell/Sell";
-import LandingPage from "./Pages/Landing";
+import LandingPage from "./Pages/landing";
 import Navbar from "./Components/navbar/Navbar";
 
 function App() {
